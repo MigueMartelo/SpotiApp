745a8afa6fd065eb2faebd10241d98c3aa10ace9
Migue Martelo <miguemartelo1031@gmail.com>
2017-09-03 08:18:40 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componets/home/home.component';
+import { SearchComponent } from './componets/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
